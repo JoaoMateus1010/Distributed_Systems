dd434821d2f387357a29e0d7c281c2f776f19f0e
João Mateus <37592317+JoaoMateus1010@users.noreply.github.com>
2019-05-22 18:28:15 -0300

    Add files via upload

--- Trabalho_03/src/Servidor.java
+++ Trabalho_03/src/Servidor.java
@@ -1,21 +1,16 @@
 import java.net.MalformedURLException;
 import java.rmi.*;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import java.util.Arrays;
-import java.rmi.server.UnicastRemoteObject;
 public class Servidor {
     public static String NameService = "ServerJM";
     public static void main(String[] args) {
         System.setSecurityManager(new SecurityManager());
         try{
-            int port = 8512;
             RemoteInterface ri = new Servente();
-            Registry registry = LocateRegistry.createRegistry(port);
             Naming.rebind(NameService, ri);
-            System.out.println("Executando Sevidor...\nNome do Serviço:"+NameService);
+            System.out.println("Executando Sevidor...\nNome do Serviço: "+NameService);
         }catch(MalformedURLException | RemoteException e){
-            System.out.println(e.getMessage());
+            System.out.println("Conexão recusada, RMI não iniciado");
+            System.exit(0);
         }
     }
 }
--- Trabalho_03/src/ShapeListClient.java
+++ Trabalho_03/src/ShapeListClient.java
@@ -1,19 +1,20 @@
 import java.io.File;
 import java.net.MalformedURLException;
 import java.rmi.*;
-import java.awt.*;
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 public class ShapeListClient {
     public static void main(String[] args) {
+
         System.setSecurityManager(new SecurityManager());        
         RemoteInterface ri = null;
         Scanner scanner = new Scanner(System.in);
-        try {            
+
+        try {
+            System.out.print("INFO :");
             System.out.println(Arrays.toString(Naming.list("ServerJM")));
             ri=(RemoteInterface)Naming.lookup("ServerJM");
+            System.out.print("STATUS : ");
             if(!ri.getStatus().equals("Ok")){
                 System.out.println("Not found Service");
                 System.exit(0);
@@ -28,27 +29,37 @@ public class ShapeListClient {
             * */
             while(true){
                 System.out.println("1- Ver próprios arquivos\n2- Ver arquivos do servidor\n3- Baixar arquivo\n4- Sair");
+                System.out.print("Waiting: ");
                 op = scanner.nextInt();
                 switch (op){
                     case 1:
-                        System.out.println("----------");
-                        for(File file:ri.getListFiles("FILES_CLIENT")){
-                            System.out.println(file.getName());
+                        System.out.println("---------- Client Files ----------");
+                        if(ri.getListFiles("FILES_CLIENT").length==0){
+                            System.out.println("Empty");
+                        }else{
+                            for(File file:ri.getListFiles("FILES_CLIENT")){
+                                System.out.println(file.getName());
+                            }
                         }
-                        System.out.println("----------");
+                        System.out.println("----------------------------------");
                         break;
                     case 2:
-                        System.out.println("----------");
-                        for(File file:ri.getListFiles("FILES_SERVER")){
-                            System.out.println(file.getName());
+                        System.out.println("---------- Server Files ----------");
+                        if(ri.getListFiles("FILES_SERVER").length==0){
+                            System.out.println("Empty");
+                        }else{
+                            for(File file:ri.getListFiles("FILES_SERVER")){
+                                System.out.println(file.getName());
+                            }
                         }
-                        System.out.println("----------");
+                        System.out.println("----------------------------------");
                         break;
                     case 3:
                         String nameFileToTake = null;
+                        System.out.print("waiting name file: ");
                         nameFileToTake = scanner.next();
                         if(ri.cpFileServer(nameFileToTake)){
-                            System.out.println("Transferencia realizada com sucesso!");
+                            System.out.println("--> Transferencia realizada com sucesso!");
                         }else{
                             System.out.println("Erro na transferencia");
                         }
@@ -60,6 +71,7 @@ public class ShapeListClient {
             }
         } catch (MalformedURLException | NotBoundException | RemoteException e){
             System.out.println("Server não encontrado");
+            System.exit(0);
         }
     }
 }
